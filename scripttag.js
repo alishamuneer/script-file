e20f68d71696d44ced0f48f4d6d542d01e9681da
Alisha <112392308+alishamuneer@users.noreply.github.com>
2023-01-31 15:34:12 +0500

    Add files via upload

---
+++ scripttag.js
@@ -0,0 +1,4 @@
+console.log('this is me script tag!!!!!!!!')
+const header = $('header.site-header').parent();
+
+header.prepend('<div>Hello this is coming from the public folder </div>').css({'background-color':'orange', 'text-align': 'center'})
