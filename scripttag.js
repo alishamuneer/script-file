05e17bd6dc1bfcb558b9c48d617633ff0b6338ab
Alisha <112392308+alishamuneer@users.noreply.github.com>
2023-02-01 19:06:30 +0500

    Update scripttag.js

--- scripttag.js
+++ scripttag.js
@@ -1,4 +1,4 @@
 console.log('this is me script tag!!!!!!!!')
 
-const form = $('h1').parent(".script-tag");
-form.prepend('<div>Script Tag Content</div>').css({'background-color':'orange', 'text-align': 'center'})
+const form = $('.script-tag');
+form.prepend('<div>Script Tag div</div>').css({'background-color':'orange', 'text-align': 'center'})
