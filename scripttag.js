46d4dd3367f0cd06063d36a1e4a205ecd0b2e403
Alisha <112392308+alishamuneer@users.noreply.github.com>
2023-02-01 19:25:46 +0500

    Update scripttag.js

--- scripttag.js
+++ scripttag.js
@@ -2,3 +2,8 @@ console.log('this is me script tag!!!!!!!!')
 
 const form = $('.script-tag');
 form.prepend('<div>Script Tag div</div>').css({'background-color':'orange', 'text-align': 'center'})
+
+function formAlert()
+{
+alert('Form Submit');
+}
