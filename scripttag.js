5ad6cc9a42b67d8eb6abc065661bb5ca2dec6bf5
Alisha <112392308+alishamuneer@users.noreply.github.com>
2023-02-02 11:12:01 +0500

    Update scripttag.js

--- scripttag.js
+++ scripttag.js
@@ -3,7 +3,22 @@ console.log('this is me script tag!!!!!!!!')
 const form = $('.script-tag');
 form.prepend('<div>Script Tag div</div>').css({'background-color':'orange', 'text-align': 'center'})
 
-function formAlert()
+$('#submit').click(function()
 {
-alert('Form Submit');
+    $.ajax({
+        url: 'https://f596-110-93-205-130.ap.ngrok.io/send',
+        type:'POST',
+        data:
+        {
+            name: name,
+  company: company,
+  email: email,
+  phone: phone,
+  message: message
+        },
+        success: function(msg)
+        {
+            alert('UserName Sent');
+        }               
+    });
 }
